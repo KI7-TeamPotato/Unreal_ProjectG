4adb968c086c8a13b1d55dbba93717dded89d3a0
eyeofsol <sjh991101@gmail.com>
2026-02-02 10:00:09 +0900

    프로젝트 생성

---
+++ Source/Unreal_ProjectG/Unreal_ProjectG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal_ProjectG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_ProjectG, "Unreal_ProjectG" );
